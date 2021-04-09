1f5281e31e44ef7a42b882a5f9f4e3cab9737262
jonathan <jhector39@gmail.com>
2021-04-08 23:57:19 -0400

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener(`DOMContentLoaded`, function(){
+    textContent()
+});
+
+
+var text = document.getElementById(`text`);
+
+function textContent (){
+    text.innerHTML = `This is really cool!`;
+    return textContent;
+}
